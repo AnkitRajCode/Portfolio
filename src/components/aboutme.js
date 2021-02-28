d2f4307a30c6b07397886270d5ee0ba298cc0a76
AnkitRaj <1906534@kiit.ac.in>
2021-02-28 20:12:00 +0530

    resume updated & instagram link bug fixed

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -10,13 +10,13 @@ class aboutme extends Component {
                <div className="intro_title">Full Stack Web Developer</div>
                <p className="intro_details">
                   I started web development 2 years ago. I basically use MERN Stack for my websites,
-                  I learned MERN Stack 1 year ago and work on 2-3 different projects based on it.
+                  I learned MERN Stack 1 year ago and worked on 2-3 different projects based on it.
                   I like to explore new projects and always want to contribute to them which helps me grow my skills efficiently.
                   My main focus is to learn algorithms and apply them in real-life projects which will enhance my skills.
                   I am also part of some coding clubs of my institute such as ACM KIIT and GFG KIIT, at GFG KIIT 
-                  I am a web dev. lead where I and my team are working on various projects which are also helping me to grow in this field.
+                  I am leading web development department where I and my team are working on various projects which are also helping me to grow in this field.
                </p>
-               <a href="https://drive.google.com/file/d/12FsP05ea6IUXy-ZT706pBaThjBYNTgeR/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="code_button"><i class="fas fa-download"></i> Resume</a>
+               <a href="https://drive.google.com/file/d/1Ir4Fq6gFPEg3Enywa8genJKwkhrWJc8O/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="code_button"><i class="fas fa-download"></i> Resume</a>
             </div>
         </div>
       )
--- src/components/footer.js
+++ src/components/footer.js
@@ -16,7 +16,7 @@ class footer extends Component {
                <div class="collab_title">For Collaboration</div>
                <a href="tel:+919431231375" class="collab_detail">
                   <lable className="collab_tel">Tel :</lable> +91 9431231375 
-                  <a href="https://api.whatsapp.com/send?phone=+919431231375&text=Hi,%20I%20would%20like%20to%20Collabrate%20with%20you.." class="redirect_whatsapp">
+                  <a target="_blank" rel="noopener noreferrer" href="https://api.whatsapp.com/send?phone=+919431231375&text=Hi,%20I%20would%20like%20to%20Collabrate%20with%20you.." class="redirect_whatsapp">
                      <img src={whatsapp} alt="whatsapp logo" class="whatsapp_logo"/> Whatsapp</a>
                </a>
                <a href="mailto:rajankit432@gmail.com?subject=Hey,%20Ankit%20Raj" class="collab_detail">
@@ -27,11 +27,11 @@ class footer extends Component {
             <div class="copyright">
                <div>
                   <a href="https://www.facebook.com/profile.php?id=100008053720762" target="_blank" rel="noopener noreferrer" title="Facebook"><img src={facebook}  alt="socialMediaLogo" className="social_media_logo"/></a>
-                  <a href="https://instagram.com/a.n.k.i.t_r.a.j_?igshid=g421khvbs56p" target="_blank" rel="noopener noreferrer" title="Instagram"><img src={instagram}  alt="socialMediaLogo" className="social_media_logo"/></a>
-                  <a href="https://twitter.com/ankit__raj__?s=08" target="_blank" rel="noopener noreferrer" title=""><img src={twitter}  alt="socialMediaLogo" className="social_media_logo"/></a>
-                  <a href="https://www.linkedin.com/in/ankit-raj-a3405b1b0" target="_blank" rel="noopener noreferrer" title=""><img src={linkedin}  alt="socialMediaLogo" className="social_media_logo"/></a>
-                  <a href="https://github.com/AnkitRajCode" target="_blank" rel="noopener noreferrer" title=""><img src={github}  alt="socialMediaLogo" className="social_media_logo"/></a>
-                  <a href="mailto:rajankit432@gmail.com?subject=Hey,%20Ankit%20Raj" target="_blank" rel="noopener noreferrer" title=""><img src={gmail}  alt="socialMediaLogo" className="social_media_logo"/></a>
+                  <a href="https://instagram.com/ankit_r.a.j?igshid=895xw0qgqmx2" target="_blank" rel="noopener noreferrer" title="Instagram"><img src={instagram}  alt="socialMediaLogo" className="social_media_logo"/></a>
+                  <a href="https://twitter.com/ankit__raj__?s=08" target="_blank" rel="noopener noreferrer" title="Twitter"><img src={twitter}  alt="socialMediaLogo" className="social_media_logo"/></a>
+                  <a href="https://www.linkedin.com/in/ankit-raj-a3405b1b0" target="_blank" rel="noopener noreferrer" title="Linkedin"><img src={linkedin}  alt="socialMediaLogo" className="social_media_logo"/></a>
+                  <a href="https://github.com/AnkitRajCode" target="_blank" rel="noopener noreferrer" title="Github"><img src={github}  alt="socialMediaLogo" className="social_media_logo"/></a>
+                  <a href="mailto:rajankit432@gmail.com?subject=Hey,%20Ankit%20Raj" target="_blank" rel="noopener noreferrer" title="Gmail"><img src={gmail}  alt="socialMediaLogo" className="social_media_logo"/></a>
                </div><br />
                <div className="copyright_text">All the information given above are 100% correct.</div>
                <div className="copyright_logo">AnkitRaj &#xa9; 2021</div>
